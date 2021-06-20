74f1397868bb20c330a5fb72c6f7f25745e10eb0
nkwoguctl <charlesdanielsctl@gmail.com>
2021-06-19 23:38:22 -0700

    .

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,10 @@ int main(void)
 	for (i = 97; i <= 122; i++)
        {
 	        if (i == 101 || i == 113)
+	  	{		
 			continue;
+
+                }
                 char charValue = i;
 		putchar(charValue);
        }

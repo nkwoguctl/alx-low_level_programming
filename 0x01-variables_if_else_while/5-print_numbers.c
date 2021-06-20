fb617492e852e8521ae657805d7399054c615805
nkwoguctl <charlesdanielsctl@gmail.com>
2021-06-20 00:06:52 -0700

    .

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<stdio.h>
+/* Prints out numbers starting from 0 and
+ * ending at 9
+ */
+int main(void)
+{
+	
+	int i;
+
+	for (i = 0; i < 10; i++)
+       {
+
+		printf("%d", i);
+       }
+       printf("\n");	
+       return(0);
+}

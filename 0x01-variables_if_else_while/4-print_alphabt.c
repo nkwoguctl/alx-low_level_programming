b3153fc0d560739ea945c7e0ad342bd92ab597e0
nkwoguctl <charlesdanielsctl@gmail.com>
2021-06-19 23:34:37 -0700

    .

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include<stdio.h>
+/* Prints out alphabets omitting 
+ * letters q and e*
+ * */
+int main(void)
+{
+	
+	int i;
+
+	for (i = 97; i <= 122; i++)
+       {
+	        if (i == 101 || i == 113)
+			continue;
+                char charValue = i;
+		putchar(charValue);
+       }
+       putchar('\n');	
+       return(0);
+}

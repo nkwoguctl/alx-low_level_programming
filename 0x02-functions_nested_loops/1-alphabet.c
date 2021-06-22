12d923d485ca5e4d4203a8d2ab0aa01902aafcaa
nkwoguctl <charlesdanielsctl@gmail.com>
2021-06-22 10:44:39 -0700

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,16 @@
 #include "holberton.h"
+/**
+ * main - prints the alphabet in lowercase
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
 	char letter;
 	
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		_putchar(letter);
-		_putchar('\n');
+		_putchar(letter)
 	}
-	return;
+	_putchar('\n');
 }	

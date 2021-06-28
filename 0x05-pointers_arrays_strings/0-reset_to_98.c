abb75011f4d3209beb2049e6066d101a27f07d4b
nkwoguctl <charlesdanielsctl@gmail.com>
2021-06-28 06:32:27 -0700

    .

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 
-
 /*
- * Function to update the pointed value to 98
- * Return 0
- */
+* reset_to_98 - resets value of pointer to 98
+* @n: pointer to reset to 98
+*
+* Return: void
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return ;
+
 }
